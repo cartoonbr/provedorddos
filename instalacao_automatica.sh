e03337a862386018ea94ec8655a28c6f9e9b57df
cartoonbr <pedrinho_plis@hotmail.com>
2023-05-24 17:48:21 -0300

    Update instalacao_automatica.sh

--- instalacao_automatica.sh
+++ instalacao_automatica.sh
@@ -3,42 +3,44 @@
 # Instalação das dependências
 echo "Instalando dependências..."
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip python3-venv
 
 # Configuração do ambiente
 echo "Configurando ambiente..."
-mikrotik_host="192.168.88.1"
+mikrotik_host="192.168.1.1"
 mikrotik_port=8728
 mikrotik_username="admin"
-mikrotik_password="inside10"
+mikrotik_password="mypassword"
 
 db_host="localhost"
 db_port=5432
-db_name="ddosprovedor"
-db_username="root"
-db_password="manu031290"
+db_name="meu_banco_de_dados"
+db_username="meu_usuario"
+db_password="minha_senha"
 
-smtp_server="smtp.gmail.com"
+smtp_server="smtp.example.com"
 smtp_port=587
-smtp_username="appboxplayer@gmail.com"
-smtp_password="#Manu51381345"
+smtp_username="seu_usuario"
+smtp_password="sua_senha"
 
 # Configuração da API da OpenAI
 echo "Configurando API da OpenAI..."
-openai_api_key="KgBGUALUaHXAYSSOn5m3T3BlbkFJvHxrCbG0LxmOXWA145N0"
+openai_api_key="sua_chave_de_api"
 
-# Configuração da chave de API da OpenAI
-echo '{"api_key": "'"$openai_api_key"'"}' > openai_secret.json
+# Configuração do ambiente virtual
+echo "Configurando ambiente virtual..."
+python3 -m venv myenv
+source myenv/bin/activate
 
-# Instalação do pacote openai_secret_manager
-echo "Instalando pacote openai_secret_manager..."
-pip3 install openai_secret_manager
+# Instalação das bibliotecas Python
+echo "Instalando bibliotecas Python..."
+pip install numpy tensorflow keras scikit-learn psycopg2-binary openai openai_secret_manager
 
 # Configuração do openai_secret_manager
 openai_secret_name="openai"
-openai_secret_key="KgBGUALUaHXAYSSOn5m3T3BlbkFJvHxrCbG0LxmOXWA145N0"
+openai_secret_key="sua_chave_de_api"
 
-# Configuração da chave de segredo da OpenAI
+# Configuração da chave de API da OpenAI
 echo '{"api_key": "'"$openai_secret_key"'"}' > openai_secret.json
 
 # Configuração do openai_secret_manager
@@ -48,12 +50,8 @@ openai_secret_manager set $openai_secret_name --file openai_secret.json
 echo "Baixando o script..."
 curl -O https://example.com/meu_script.py
 
-# Instalação das bibliotecas Python
-echo "Instalando bibliotecas Python..."
-pip3 install numpy tensorflow keras scikit-learn psycopg2 openai openai_secret_manager
-
 # Execução do script
 echo "Executando o script..."
-python3 meu_script.py
+python meu_script.py
 
 echo "Instalação concluída."
