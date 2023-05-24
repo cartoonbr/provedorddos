55629c4c44e1186ede35a4e27107313c38c9628e
cartoonbr <pedrinho_plis@hotmail.com>
2023-05-24 16:03:33 -0300

    Create meu_script.py

---
+++ meu_script.py
@@ -0,0 +1,207 @@
+import socket
+import threading
+import logging
+import os
+import openai_secret_manager
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import openai
+import psycopg2
+
+# Recupera a chave de API da OpenAI
+secrets = openai_secret_manager.get_secret("openai")
+api_key = secrets["api_key"]
+openai.api_key = api_key
+
+# Configuração do logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Configuração do servidor MikroTik
+mikrotik_host = "192.168.1.1"
+mikrotik_port = 8728
+mikrotik_username = "admin"
+mikrotik_password = "mypassword"
+
+# Configuração do modelo de detecção de anomalias
+model_path = 'models/anomaly_detector.h5'
+scaler_mean_path = 'models/scaler_mean.npy'
+scaler_std_path = 'models/scaler_std.npy'
+
+# Configuração do servidor de monitoramento
+monitor_host = "0.0.0.0"
+monitor_port = 80
+monitor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+monitor_socket.bind((monitor_host, monitor_port))
+monitor_socket.listen(5)
+
+# Configuração do banco de dados
+db_host = "localhost"
+db_port = 5432
+db_name = "traffic_db"
+db_user = "db_user"
+db_password = "db_password"
+
+# Lista para armazenar os dados do tráfego de rede
+traffic_data = []
+
+# Informações do servidor SMTP
+smtp_server = "smtp.example.com"
+smtp_port = 587
+smtp_username = "your_username"
+smtp_password = "your_password"
+
+# Carregar modelo de detecção de anomalias e scaler
+anomaly_detector = keras.models.load_model(model_path)
+scaler_mean = np.load(scaler_mean_path)
+scaler_std = np.load(scaler_std_path)
+scaler = StandardScaler()
+scaler.mean_ = scaler_mean
+scaler.scale_ = scaler_std
+
+# Conexão com o banco de dados
+db_connection = psycopg2.connect(
+    host=db_host,
+    port=db_port,
+    database=db_name,
+    user=db_user,
+    password=db_password
+)
+
+def block_ip(ip_address):
+    """
+    Bloqueia um endereço IP no servidor MikroTik.
+    """
+    logger.info(f"Bloqueando endereço IP {ip_address}...")
+    os.system(f"/ip firewall address-list add list=blocked address={ip_address}")
+
+def unblock_ip(ip_address):
+    """
+    Desbloqueia um endereço IP no servidor MikroTik.
+    """
+    logger.info(f"Desbloqueando endereço IP {ip_address}...")
+    os.system(f"/ip firewall address-list remove [find address={ip_address}]")
+
+def notify_admin(subject, message):
+    """
+    Envia uma notificação para o administrador do sistema.
+    """
+    send_email(subject, message, "from@example.com", "to@example.com")
+
+def send_email(subject, message, from_email, to_email):
+    """
+    Envia uma notificação por e-mail.
+    """
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    # Corpo do e-mail
+    body = message
+    msg.attach(MIMEText
+    body, 'plain'))
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.send_message(msg)
+        server.quit()
+        logger.info("Notificação por e-mail enviada com sucesso.")
+    except Exception as e:
+        logger.error(f"Erro ao enviar notificação por e-mail: {str(e)}")
+
+def detect_anomaly(data):
+    """
+    Detecta anomalias no tráfego de rede utilizando um modelo de machine learning.
+    """
+    data_scaled = scaler.transform(data)
+    predictions = anomaly_detector.predict(data_scaled)
+    return np.mean(predictions)
+
+def update_model():
+    """
+    Atualiza o modelo de detecção de anomalias com novos dados e salva o modelo atualizado.
+    """
+    # Recupera os dados de tráfego de rede
+    traffic_data = retrieve_traffic_data()
+
+    # Treina um novo modelo com base nos dados atualizados
+    new_model = train_anomaly_detection_model(traffic_data)
+
+    # Salva o novo modelo
+    save_model(new_model)
+
+def analyze_text(text):
+    """
+    Analisa o texto utilizando a OpenAI API.
+    """
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=text,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+
+    if response and len(response.choices) > 0:
+        return response.choices[0].text.strip()
+    else:
+        return None
+
+def process_log(log):
+    """
+    Processa um log de tráfego de rede.
+    """
+    # Realiza análise de texto do log
+    analysis = analyze_text(log)
+
+    # Executa ações com base na análise
+    if analysis:
+        if "anomalia" in analysis:
+            block_ip(log.ip_address)
+            notify_admin("Detecção de Anomalia", f"Uma anomalia foi detectada no endereço IP {log.ip_address}. O IP foi bloqueado.")
+        else:
+            unblock_ip(log.ip_address)
+            notify_admin("Anomalia Resolvida", f"A anomalia no endereço IP {log.ip_address} foi resolvida. O IP foi desbloqueado.")
+            
+# Funções para armazenar e recuperar dados de tráfego no banco de dados
+
+def store_traffic_data(data):
+    """
+    Armazena os dados de tráfego no banco de dados.
+    """
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute("INSERT INTO traffic_data (timestamp, data) VALUES (NOW(), %s)", (data,))
+        db_connection.commit()
+        logger.info("Dados de tráfego armazenados com sucesso.")
+    except Exception as e:
+        logger.error(f"Erro ao armazenar dados de tráfego: {str(e)}")
+
+def retrieve_traffic_data():
+    """
+    Recupera os dados de tráfego do banco de dados.
+    """
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute("SELECT data FROM traffic_data")
+        rows = cursor.fetchall()
+        traffic_data = [row[0] for row in rows]
+        logger.info("Dados de tráfego recuperados com sucesso.")
+        return traffic_data
+    except Exception as e:
+        logger.error(f"Erro ao recuperar dados de tráfego: {str(e)}")
+        return []
+
+# Restante do código...
+
+# Inicie a execução do servidor de monitoramento em uma nova thread
+monitor_thread = threading.Thread(target=monitor_traffic)
+monitor_thread
