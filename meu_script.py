ad6da98b449dcdcacd5300de7ac6235ff4142e04
cartoonbr <pedrinho_plis@hotmail.com>
2023-05-25 10:51:13 -0300

    Update meu_script.py

--- meu_script.py
+++ meu_script.py
@@ -13,21 +13,16 @@ import openai
 import psycopg2
 
 # Recupera a chave de API da OpenAI
-secrets = openai_secret_manager.get_secret("openai")
-api_key = secrets["api_key"]
-openai.api_key = api_key
+api_key = None
 
-# Restante do código...
+try:
+    secrets = openai_secret_manager.get_secret("openai")
+    api_key = secrets["api_key"]
+except Exception as e:
+    print(f"Erro ao recuperar a chave de API da OpenAI: {str(e)}")
 
-# Define uma função auxiliar para obter segredos
-def get_secret(key_name):
-    """
-    Obtém um segredo do gerenciador de segredos.
-    """
-    return openai_secret_manager.get_secret(key_name)
-
-# Atualiza a implementação da função get_secret
-openai_secret_manager.get_secret = get_secret
+if api_key:
+    openai.api_key = api_key
 
 # Configuração do logger
 logging.basicConfig(level=logging.INFO)
@@ -68,21 +63,33 @@ smtp_username = "your_username"
 smtp_password = "your_password"
 
 # Carregar modelo de detecção de anomalias e scaler
-anomaly_detector = keras.models.load_model(model_path)
-scaler_mean = np.load(scaler_mean_path)
-scaler_std = np.load(scaler_std_path)
+anomaly_detector = None
+scaler_mean = None
+scaler_std = None
 scaler = StandardScaler()
-scaler.mean_ = scaler_mean
-scaler.scale_ = scaler_std
+
+try:
+    anomaly_detector = keras.models.load_model(model_path)
+    scaler_mean = np.load(scaler_mean_path)
+    scaler_std = np.load(scaler_std_path)
+    scaler.mean_ = scaler_mean
+    scaler.scale_ = scaler_std
+except Exception as e:
+    print(f"Erro ao carregar modelo de detecção de anomalias e scaler: {str(e)}")
 
 # Conexão com o banco de dados
-db_connection = psycopg2.connect(
-    host=db_host,
-    port=db_port,
-    database=db_name,
-    user=db_user,
-    password=db_password
-)
+db_connection = None
+
+try:
+    db_connection = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        database=db_name,
+        user=db_user,
+        password=db_password
+    )
+except Exception as e:
+    print(f"Erro ao conectar ao banco de dados: {str(e)}")
 
 def block_ip(ip_address):
     """
@@ -98,25 +105,25 @@ def unblock_ip(ip_address):
     logger.info(f"Desbloqueando endereço IP {ip_address}...")
     os.system(f"/ip firewall address-list remove [find address={ip_address}]")
 
-def notify_admin(subject, message):
-    """
-    Envia uma notificação para o administrador do sistema.
-    """
-    send_email(subject, message, "from@example.com", "to@example.com")
+def notify_admin
+(subject, message):
+"""
+Envia uma notificação para o administrador do sistema.
+"""
+send_email(subject, message, "from@example.com", "to@example.com")
 
 def send_email(subject, message, from_email, to_email):
-    """
-    Envia uma notificação por e-mail.
-    """
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
+"""
+Envia uma notificação por e-mail.
+"""
+msg = MIMEMultipart()
+msg['From'] = from_email
+msg['To'] = to_email
+msg['Subject'] = subject
 
-    # Corpo do e-mail
-    body = message
+# Corpo do e-mail
+body = message
 msg.attach(MIMEText(body, 'plain'))
-
 
 try:
     server = smtplib.SMTP(smtp_server, smtp_port)
@@ -127,95 +134,137 @@ try:
     logger.info("Notificação por e-mail enviada com sucesso.")
 except Exception as e:
     logger.error(f"Erro ao enviar notificação por e-mail: {str(e)}")
-
-
-
 def detect_anomaly(data):
-    """
-    Detecta anomalias no tráfego de rede utilizando um modelo de machine learning.
-    """
-    data_scaled = scaler.transform(data)
-    predictions = anomaly_detector.predict(data_scaled)
-    return np.mean(predictions)
-
+"""
+Detecta anomalias no tráfego de rede utilizando um modelo de machine learning.
+"""
+if anomaly_detector is None or scaler_mean is None or scaler_std is None:
+return None data_scaled = scaler.transform(data)
+predictions = anomaly_detector.predict(data_scaled)
+return np.mean(predictions)
+data_scaled = scaler.transform(data)
+predictions = anomaly_detector.predict(data_scaled)
+return np.mean(predictions)
 def update_model():
-    """
-    Atualiza o modelo de detecção de anomalias com novos dados e salva o modelo atualizado.
-    """
-    # Recupera os dados de tráfego de rede
-    traffic_data = retrieve_traffic_data()
+"""
+Atualiza o modelo de detecção de anomalias com novos dados e salva o modelo atualizado.
+"""
+if db_connection is None:
+return
 
-    # Treina um novo modelo com base nos dados atualizados
-    new_model = train_anomaly_detection_model(traffic_data)
+# Recupera os dados de tráfego de rede
+traffic_data = retrieve_traffic_data()
 
-    # Salva o novo modelo
-    save_model(new_model)
+# Treina um novo modelo com base nos dados atualizados
+new_model = train_anomaly_detection_model(traffic_data)
 
+# Salva o novo modelo
+save_model(new_model)
 def analyze_text(text):
-    """
-    Analisa o texto utilizando a OpenAI API.
-    """
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=text,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
+"""
+Analisa o texto utilizando a OpenAI API.
+"""
+if api_key is None:
+return None 
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=text,
+    max_tokens=100,
+    n=1,
+    stop=None,
+    temperature=0.5
+)
 
-    if response and len(response.choices) > 0:
-        return response.choices[0].text.strip()
-    else:
-        return None
+if response and len(response.choices) > 0:
+    return response.choices[0].text.strip()
+else:
+    return None
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=text,
+    max_tokens=100,
+    n=1,
+    stop=None,
+    temperature=0.5
+)
 
+if response and len(response.choices) > 0:
+    return response.choices[0].text.strip()
+else:
+    return None
 def process_log(log):
-    """
-    Processa um log de tráfego de rede.
-    """
-    # Realiza análise de texto do log
-    analysis = analyze_text(log)
+"""
+Processa um log de tráfego de rede.
+"""
+# Realiza análise de texto do log
+analysis = analyze_text(log)
 
-    # Executa ações com base na análise
-    if analysis:
-        if "anomalia" in analysis:
-            block_ip(log.ip_address)
-            notify_admin("Detecção de Anomalia", f"Uma anomalia foi detectada no endereço IP {log.ip_address}. O IP foi bloqueado.")
-        else:
-            unblock_ip(log.ip_address)
-            notify_admin("Anomalia Resolvida", f"A anomalia no endereço IP {log.ip_address} foi resolvida. O IP foi desbloqueado.")
-            
-# Funções para armazenar e recuperar dados de tráfego no banco de dados
-
+# Executa ações com base na análise
+if analysis:
+    if "anomalia" in analysis:
+        block_ip(log.ip_address)
+        notify_admin("Detecção de Anomalia", f"Uma anomalia foi detectada no endereço IP {log.ip_address}. O IP foi bloqueado.")
+    else:
+        unblock_ip(log.ip_address)
+        notify_admin("Anomalia Resolvida", f"A anomalia no endereço IP {log.ip_address} foi resolvida. O IP foi desbloqueado.")
 def store_traffic_data(data):
-    """
-    Armazena os dados de tráfego no banco de dados.
-    """
-    try:
-        cursor = db_connection.cursor()
-        cursor.execute("INSERT INTO traffic_data (timestamp, data) VALUES (NOW(), %s)", (data,))
-        db_connection.commit()
-        logger.info("Dados de tráfego armazenados com sucesso.")
-    except Exception as e:
-        logger.error(f"Erro ao armazenar dados de tráfego: {str(e)}")
+"""
+Armazena os dados de tráfego no banco de dados.
+"""
+if db_connection is None:
+return
+
+try:
+    cursor = db_connection.cursor()
+    for log in data:
+        cursor.execute("INSERT INTO traffic_logs (ip_address, timestamp, log_data) VALUES (%s, %s, %s)",
+                       (log.ip_address, log.timestamp, log.log_data))
+    db_connection.commit()
+    logger.info("Dados de
+tráfego armazenados com sucesso no banco de dados.")
+except Exception as e:
+logger.error(f"Erro ao armazenar dados de tráfego no banco de dados: {str(e)}")
 
 def retrieve_traffic_data():
-    """
-    Recupera os dados de tráfego do banco de dados.
-    """
-    try:
-        cursor = db_connection.cursor()
-        cursor.execute("SELECT data FROM traffic_data")
-        rows = cursor.fetchall()
-        traffic_data = [row[0] for row in rows]
-        logger.info("Dados de tráfego recuperados com sucesso.")
-        return traffic_data
-    except Exception as e:
-        logger.error(f"Erro ao recuperar dados de tráfego: {str(e)}")
-        return []
+"""
+Recupera os dados de tráfego armazenados no banco de dados.
+"""
+if db_connection is None:
+return []
 
-# Restante do código...
+try:
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT ip_address, timestamp, log_data FROM traffic_logs")
+    rows = cursor.fetchall()
+    traffic_data = []
+    for row in rows:
+        ip_address, timestamp, log_data = row
+        log = TrafficLog(ip_address, timestamp, log_data)
+        traffic_data.append(log)
+    return traffic_data
+except Exception as e:
+    logger.error(f"Erro ao recuperar dados de tráfego do banco de dados: {str(e)}")
+    return []
+class TrafficLog:
+def init(self, ip_address, timestamp, log_data):
+self.ip_address = ip_address
+self.timestamp = timestamp
+self.log_data = log_data
 
-# Inicie a execução do servidor de monitoramento em uma nova thread
+def monitor_traffic():
+while True:
+client_socket, address = monitor_socket.accept()
+logger.info(f"Nova conexão de {address[0]}:{address[1]}")
+data = client_socket.recv(1024)
+log = data.decode("utf-8")
+logger.info(f"Log de tráfego recebido: {log}")
+process_log(log)
+client_socket.close()
+
 monitor_thread = threading.Thread(target=monitor_traffic)
-monitor_thread
+monitor_thread.start()
+
+if name == "main":
+# Configurações e preparações iniciais
+update_model()
+# Outras inicializações...
